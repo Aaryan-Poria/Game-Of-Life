4a44c22aacc66ff647bebf3290810942a6036259
Aaryan Poria <aaryanporia@gmail.com>
2023-07-04 12:13:07 +0530

    Wrote a function to expand the start board

--- main.py
+++ main.py
@@ -27,6 +27,27 @@ def random_state(rows, columns):
     return board_state
 
 
+def expand_board(input_board, num_rows, num_cols):
+    # Step 1: Determine the current dimensions of the input board
+    curr_rows = len(input_board)
+    curr_cols = len(input_board[0])
+
+    # Step 2: Calculate the desired dimensions for the expanded board
+    expanded_rows = curr_rows + 2 * num_rows
+    expanded_cols = curr_cols + 2 * num_cols
+
+    # Step 3: Create a new expanded board with the desired dimensions
+    expanded_board = [[0] * expanded_cols for _ in range(expanded_rows)]
+
+    # Step 4: Copy the contents of the input board into the expanded board
+    for i in range(curr_rows):
+        for j in range(curr_cols):
+            expanded_board[i + num_rows][j + num_cols] = input_board[i][j]
+
+    # Step 5: Return the expanded board
+    return expanded_board
+
+
 def calc_neighbours(board_state, i, j):
     alive_neighbours = 0
     for x in range(-1, 2):
@@ -85,18 +106,20 @@ def read_from_file():
 random.seed(None, version=2)
 
 start_state = read_from_file()
-height = len(start_state)
-width = len(start_state[0])
+start_state = expand_board(start_state, 5, 5)
 
-render(start_state, height, width)
+current_rows = len(start_state)
+current_cols = len(start_state[0])
+
+render(start_state, current_rows, current_cols)
 
 while True:
     input("Press Enter to generate the next state: ")
-    new_state = next_board_state(start_state, height, width)
+    new_state = next_board_state(start_state, current_rows, current_cols)
     if os.name == "nt":
         _ = os.system("cls")
     else:
         _ = os.system("clear")
 
-    render(new_state, height, width)
+    render(new_state, current_rows, current_cols)
     start_state = new_state
